774218a841a5821480d14df38846b0113011df70
Patrick Elsen <pelsen@xfbs.net>
2019-07-29 12:31:38 +0200

    adds json-builder package

---
+++ Formula/json-builder.rb
@@ -0,0 +1,19 @@
+class JsonBuilder < Formula
+  desc "The serializing counterpart to json-parser."
+  homepage "https://github.com/udp/json-builder"
+  revision 1
+
+  head "https://github.com/udp/json-builder.git"
+
+  depends_on "cmake" => :build
+  depends_on "json-parser"
+
+  def install
+    args = %W[
+      -DCMAKE_C_FLAGS="-I/usr/local/include/json-parser"
+    ]
+
+    system "cmake", ".", *std_cmake_args, *args
+    system "make", "install"
+  end
+end
